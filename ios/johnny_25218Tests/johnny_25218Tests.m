0425dd1555e400d7a997e6dae630e22a08e4ad39
Crowdbotics <team@crowdbotics.com>
2021-03-23 20:11:56 +0000

    Initial commit.

---
+++ ios/johnny_25218Tests/johnny_25218Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface johnny_25218 : XCTestCase
+
+@end
+
+@implementation johnny_25218
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
---
+++ src/config/app.js
@@ -0,0 +1,5 @@
+export const appConfig = {
+  // todo add library to handle env variables
+  emailAuthAPIEndPoint: "https://johnny-25218.botics.co",
+  defaultTimeout: 5000
+};
